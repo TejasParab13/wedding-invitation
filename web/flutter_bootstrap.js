7b95a8802ecd6b6aaa324e1e7117bc180fb2d56c
Tejas <tej13parab@gmail.com>
2026-03-07 17:13:15 +0530

    new

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3695663842"
+    serviceWorkerVersion: "3529192686"
   }
 });
