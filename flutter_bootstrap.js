000e9e288d305db7d82e71859b5bfc5515bf67b6
Tejas <tej13parab@gmail.com>
2026-03-07 10:23:49 +0530

    fix 3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "66144058"
+    serviceWorkerVersion: "654332054"
   }
 });
